1f33bdf7ab3634299ed39f0e16802b9e8d36da1a
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-04-04 11:39:25 -0700

    Update isItGood.py

--- isItGood.py
+++ isItGood.py
@@ -7,23 +7,31 @@ Details:  Selecting the NOAA buoy number of your choice, we search for the raw .
 
 
 '''
-
 import requests
-# from bs4 import BeautifulSoup as bs  (only if needed)
+from bs4 import BeautifulSoup as bs
 from datetime import datetime as dt
 
 def now(): # in case we need to compare current time
     print(dt.now())
 
-def urlReq(): 
+def file_read(fname):
+    content_array = []
+    with open(fname) as f:
+        for line in f:
+            content_array.append(line)
+            textArray = open("waveArray.txt","w")
+            textArray.write("%s" % content_array)
+        print(content_array)
+
+def urlReq(): # no arguments
     buoy = "46224" #change based on your preference
     url = "https://www.ndbc.noaa.gov/data/realtime2/"+buoy+".txt"
     page = requests.get("%s" % url) #request built url
     content = page.content #content is the webpage
     content = content.decode('utf-8') # make nice
-    text_file = open("waveFile.txt", "w") #create waveFile.txt in current directory
-    text_file.write("%s" % content) # data is multi-line buoy readings
-    print(content)
+    text_file = open("waveFile.txt", "w")
+    text_file.write("%s" % content)
+    file_read("waveFile.txt")
     
-   ## TO DO: create arrays to sort through for each line to compare data
-    
+# call function
+urlReq()
