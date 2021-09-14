1add8d3da11fa1993158f3ab0d05e614c77d8057
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-09-14 06:01:50 -0700

    Add files via upload

--- send_text.py
+++ send_text.py
@@ -1,4 +1,5 @@
 '''  SAMPLE TO SEND TeXT MESSAGE
+USE FOR TESTING THIS SHOULD NOT BE INCLUDED IN ANY PROGRAM YET
 
 Try and make it OOP
 '''
@@ -6,26 +7,17 @@ Try and make it OOP
 #!/bin/python
 
 from twilio.rest import Client
-'''
-account_sid = ""# you know what it is
-auth_token = ""
-client = Client(account_sid, auth_token)
-client.messages.create(
-    to="+",#verified phone to send to most likely your own phone number
-    from_="+",#twilion phone number
-    body="Taco Fiesta A La Playa!  "
-)
-'''
+
 def send_text(true, height, period):
     if true >= 1:
         height = height
         period = period
-        account_sid = ""
-        auth_token = ""
+        account_sid = "AC6b88e76ab99311def0fb9a76bc14af51"
+        auth_token = "d5b6f2f9fb64da3b0a67278a7d1a3ad0"
         client = Client(account_sid, auth_token)
         client.messages.create(
-            to="+",#verified phone to send to most likely your own phone number
-            from_="+",#twilion phone number
+            to="+19495219358",
+            from_="+15103067958",
             body=f"Go surf the waves are {height}ft @{period}sec !! \
             Check out the latest details at http://phaseassay.com"
         )
