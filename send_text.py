36858ba3364f21b5f950e08be76ece4d90c3be34
CloudsWeight <CloudsWeight@users.noreply.github.com>
2021-09-14 06:03:06 -0700

    Update send_text.py

--- send_text.py
+++ send_text.py
@@ -1,10 +1,5 @@
-'''  SAMPLE TO SEND TeXT MESSAGE
-USE FOR TESTING THIS SHOULD NOT BE INCLUDED IN ANY PROGRAM YET
-
-Try and make it OOP
+'''  Function to send text using auth and acct id fromt twilio 
 '''
-
-#!/bin/python
 
 from twilio.rest import Client
 
@@ -12,12 +7,12 @@ def send_text(true, height, period):
     if true >= 1:
         height = height
         period = period
-        account_sid = "AC6b88e76ab99311def0fb9a76bc14af51"
-        auth_token = "d5b6f2f9fb64da3b0a67278a7d1a3ad0"
+        account_sid = ""# you know what it is
+        auth_token = ""
         client = Client(account_sid, auth_token)
         client.messages.create(
-            to="+19495219358",
-            from_="+15103067958",
+            to="+",#verified phone to send to most likely your own phone number
+            from_="+",# twilio phone number to send texts
             body=f"Go surf the waves are {height}ft @{period}sec !! \
             Check out the latest details at http://phaseassay.com"
         )
