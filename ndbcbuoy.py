3bf1eea72e50654287406aed5f365c6ac66cc548
CloudsWeight <CloudsWeight@users.noreply.github.com>
2022-01-02 12:46:56 -0800

    Update ndbcbuoy.py

--- ndbcbuoy.py
+++ ndbcbuoy.py
@@ -4,16 +4,18 @@ Creates a dictionary of swell/period for each timestamp.
 Charts recent swell and period.
 Sends a text message based on user defined paremeters.
 
-'''
-import requests
+'''import requests
 import datetime
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 class BuoyData:
-    ''' Initializes to 46224, change BuoyData.buoy to another number, and save the url, BuoyData.set_buoy_url() '''
+    ''' Initializes to buoy#46224.  To change #, set_buoy_n(#).  To save the url, set_buoy_url() '''
+
     def __init__(self):
+        self.N = 33
         self.set_buoy_n()
         self.set_buoy_url()
         self.save_content_to_file()
@@ -24,8 +26,9 @@ class BuoyData:
     def __str__(self):
         return self.url
 
-    def set_buoy_n(self, n = 46224):
+    def set_buoy_n(self, n = '46268'):
         self.buoy = n
+        self.set_buoy_url()
         return n
 
     def set_buoy_url(self):
@@ -52,9 +55,9 @@ class BuoyData:
         content = self.get_url_content()
         # create a time stamp for files if necessary, un-comment both below
         #now_str = self.get_time_str()
-        #built_ts = f'{now_str}waves.txt'
+        #self.file_name = f'{now_str}waves.txt'
         self.file_name ="waves.txt"
-        text_file = open(self.file_name, "a")
+        text_file = open(self.file_name, "w")
         text_file.write(content)
         text_file.close()
         return self.file_name
@@ -67,14 +70,17 @@ class BuoyData:
                 details_list.append(line)
         return details_list  # return data cleaning list
 
+    def set_sample_size(self, N=33):
+        self.N = N
+        return self.N
+
     def clean_list_data(self):
         details_list = self.create_list_from_content()
         # select only last N  list items for calculations
         cleaned_data = []
-        N = 33
         i = 0
         for line in details_list:
-            if i < N:
+            if i < self.N:
                 cleaned_data.append(line)
                 i += 1
         return cleaned_data
@@ -82,23 +88,31 @@ class BuoyData:
     def create_wave_dict(self):
         cleaned_data = self.clean_list_data()
         wave_data = {}
-        for i in cleaned_data[2::3]:
-            wave_data[i[4:16]] = (float("{:.2f}".format(float(i[33:36])*3.281)),float("{:.2f}".format(float(i[39:42]))))
-            #height.append(float("{:.2f}".format(float(i[33:36]))*3.281))
+        n = 0
+        for i in cleaned_data[2::2]:
+            wave_data[n] = [i[4:16], float("{:.2f}".format(float(i[33:36])*3.281)),float("{:.2f}".format(float(i[39:42])))]
+            n +=1
         return wave_data
 
+    def get_dataframe(self):
+        df = pd.DataFrame(self.create_wave_dict())
+        return df
+
     def display_wave_data(self):
-        x = []
-        y = []
+        x , y1, y2 = [], [], []
         wave_data = self.buoy_request()
+        print(wave_data.items())
         for i in wave_data.items():
-            x.append(i[0])
-            y.append(i[1])
+            x.append(i[1][0])
+            y1.append(i[1][1])
+            y2.append(i[1][2])
         x.reverse()
-        y.reverse()
-        plt.plot(x,y)
+        y1.reverse()
+        y2.reverse()
+        plt.plot(x, y1, label ='ft')
+        plt.plot(x,y2, label='sec')
         plt.show()
-        return wave_data
+
 
     def save_buoy_details(self):
             deets = self.create_list_from_content()
@@ -116,16 +130,17 @@ class BuoyData:
 
 def main():
     a = BuoyData()
-    a.display_wave_data()
-
+    print(a.display_wave_data())
 
 if __name__ =="__main__":
     main()
 
-
 '''
-https://www.ndbc.noaa.gov/to_station.shtml
+Some other working buoy numbers, becaus  not all buoy numbers work:
+    -  46268 ; Topanga Nearshore, CA
+    -  44097; Block Island, RI
+    -
 
-list of ndbc stations
+List of ndbc stations: https://www.ndbc.noaa.gov/to_station.shtml
 
 '''
