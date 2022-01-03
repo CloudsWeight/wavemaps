82b81c2652dc95dd2ab2818c45fa498f747ac674
CloudsWeight <CloudsWeight@users.noreply.github.com>
2022-01-03 04:09:55 -0800

    Update send_text.py

--- send_text.py
+++ send_text.py
@@ -1,19 +1,20 @@
-'''  Function to send text using auth and acct id fromt twilio 
-'''
-
+'''  Function to send text using auth and acct id fromt twilio'''
 from twilio.rest import Client
+from twiliokey import TwilioKey
 
-def send_text(true, height, period):
-    if true >= 1:
-        height = height
-        period = period
-        # replace account_sid and auth_token with your values
-        account_sid = "your-information"    
-        auth_token = "your-information"
-        client = Client(account_sid, auth_token)
-        client.messages.create(
-            to="+",#verified phone to send to most likely your own phone number
-            from_="+",# twilio phone number to send texts
-            body=f"Go surf the waves are {height}ft @{period}sec !! \
-            Check out the latest details at http://phaseassay.com"
-        )
+def send_text(height, period):
+    ''' Requires a height and period variable, 
+    SMS decision is based on height.  '''
+    key = TwilioKey()
+    height = float("{:.2f}".format(height))
+    period = period
+    account_sid = key.get_sid()
+    auth_token = key.get_auth()
+    client = Client(account_sid, auth_token)
+    sms_body = f"Wave Maps - Go surf! Waves are {height}ft @{period}sec \n" \
+    "- <3 your friends from http://phaseassay.com"
+    client.messages.create(
+        to="+", # this is the phone to receive the text
+        from_="+", # this is the twilio assigned SMS #
+        body= sms_body)
+    return sms_body
